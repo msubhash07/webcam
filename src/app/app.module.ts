77446c18d007558d7ff96af1e80662b390b12b90
Subhash <developer.subhash07@gmail.com>
2020-07-06 12:30:39 +0530

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {WebcamModule} from 'ngx-webcam';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	WebcamModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
